3c72fa93fe2d7fd31cd86e1b76cb542395ce67c4
Chahine-Benramoul <48608784+Chahine-Benramoul@users.noreply.github.com>
2023-11-30 17:26:08 -0500

    moved to siteUI

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -41,3 +41,118 @@ function renderAbout() {
             </div>
         `))
 }
+
+const renderFromConnection = () =>  {
+    return `
+        <div class="content" style="text-align:center">
+            <h3>Connexion</h3>
+            <form class="form" id="loginForm">
+                <input type='email'
+                name='Email'
+                class="form-control"
+                required
+                RequireMessage = 'Veuillez entrer votre courriel'
+                InvalidMessage = 'Courriel invalide'
+                placeholder="adresse de courriel"
+                value=''>
+                <span style='color:red'></span>
+                <input type='password'
+                name='Password'
+                placeholder='Mot de passe'
+                class="form-control"
+                required
+                RequireMessage = 'Veuillez entrer votre mot de passe'>
+                <span style='color:red'></span>
+                <input type='submit' name='submit' value="Entrer" class="form-control btn-primary">
+            </form>
+            <div class="form">
+                <hr>
+                <button class="form-control btn-info" id="createProfilCmd">Nouveau compte</button>
+            </div>
+        </div>
+    
+    `;
+}
+
+const renderFormInscription = () => {
+    return `
+        <script> initImageUploaders(); </script>
+        <form class="form" id="createProfilForm"'>
+        <fieldset>
+        <legend>Adresse ce courriel</legend>
+        <input type="email"
+        class="form-control Email"
+        name="Email"
+        id="Email"
+        placeholder="Courriel"
+        required
+        RequireMessage = 'Veuillez entrer votre courriel'
+        InvalidMessage = 'Courriel invalide'
+        CustomErrorMessage ="Ce courriel est déjà utilisé"/>
+        <input class="form-control MatchedInput"
+        type="text"
+        matchedInputId="Email"
+        name="matchedEmail"
+        id="matchedEmail"
+        placeholder="Vérification"
+        required
+        RequireMessage = 'Veuillez entrez de nouveau votre courriel'
+        InvalidMessage="Les courriels ne correspondent pas" />
+        </fieldset>
+        <fieldset>
+        <legend>Mot de passe</legend>
+        <input type="password"
+        class="form-control"
+        name="Password"
+        id="Password"
+        placeholder="Mot de passe"
+        required
+        RequireMessage = 'Veuillez entrer un mot de passe'
+        InvalidMessage = 'Mot de passe trop court'/>
+        <input class="form-control MatchedInput"
+        type="password"
+        matchedInputId="Password"
+        name="matchedPassword"
+        id="matchedPassword"
+        placeholder="Vérification" required
+        InvalidMessage="Ne correspond pas au mot de passe" />
+        </fieldset>
+        <fieldset>
+        <legend>Nom</legend>
+        <input type="text"
+        class="form-control Alpha"
+        name="Name"
+        id="Name"
+        placeholder="Nom"
+        required
+        RequireMessage = 'Veuillez entrer votre nom'
+        InvalidMessage = 'Nom invalide'/>
+        </fieldset>
+        <fieldset>
+        <legend>Avatar</legend>
+        <div class='imageUploader'
+        newImage='true'
+        controlId='Avatar'
+        imageSrc='../images/no-avatar.png'
+        waitingImage="../images/Loading_icon.gif">
+        </div>
+        </fieldset>
+        <input type='submit' name='submit' id='saveUserCmd' value="Enregistrer" class="form-control btn-primary">
+        </form>
+        </div>
+        <div class="cancel">
+            <button class="form-control btn-secondary" id="abortCmd">Annuler</button>
+        </div>
+    `;
+};
+
+const setUp = () => {
+    $("#content").append(renderFromConnection());
+    $('#createProfilCmd').on("click",()=>{
+        $("body").html(renderFormInscription);
+    })
+};
+
+$(() => {
+    setUp();
+});
